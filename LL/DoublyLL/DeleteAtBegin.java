99d52a057dd553e560414c318f9edbe488149e1f
Vijay Kumar <vijaykumarmganj1@gmail.com>
2024-09-08 23:36:59 +0530

    one code tested with a new input

--- LL/DoublyLL/DeleteAtBegin.java
+++ LL/DoublyLL/DeleteAtBegin.java
@@ -33,6 +33,7 @@ public class DeleteAtBegin {
         Node head = new Node(10);
         head.next = new Node(20);
         head.next.next = new Node(30);
+        head.next.next.next = new Node(80);
 
         head = deleteAtBegin(head);
         printLL(head);
@@ -40,6 +41,3 @@ public class DeleteAtBegin {
     }
 
 }
-
-
-
